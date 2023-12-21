bdb5fd71a034149f2f0e64bd8ae87f4cd7b42559
youssef <youssef.elassaoui@gmail.com>
2023-12-21 17:23:47 +0100

    creation entity,table,id and sequence annotaions

--- src/main/java/com/example/demo/models/Content.java
+++ src/main/java/com/example/demo/models/Content.java
@@ -1,4 +1,5 @@
 package com.example.demo.models;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
@@ -7,7 +8,20 @@ import lombok.AllArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+
+@Entity
+@Table
 public class Content {
+    @Id
+    @SequenceGenerator(
+            name = "content_sequence",
+            sequenceName = "content_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "content_sequence"
+    )
     private long id;
     private String name;
     private byte[] video;
--- src/main/java/com/example/demo/models/Level.java
+++ src/main/java/com/example/demo/models/Level.java
@@ -1,4 +1,5 @@
 package com.example.demo.models;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
@@ -7,7 +8,20 @@ import lombok.AllArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+
+@Entity
+@Table
 public class Level {
+    @Id
+    @SequenceGenerator(
+            name = "level_sequence",
+            sequenceName = "level_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "level_sequence"
+    )
     private long id;
     private String abreviationLevel;
 }
